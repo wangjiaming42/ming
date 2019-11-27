12d1575e304fa4bada58d3119b208ed26b90da41
wangjiaming <875532905@qq.com>
2019-11-27 10:25:30 +0800

    Merge branch 'master' of https://github.com/wangjiaming42/ming
    # Conflicts:
    #	src/Test/Test.java

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,6 +2,10 @@ package Test;
 
 public class Test {
      public static void main(String[] args) {
+<<<<<<< .merge_file_a02324
+    	 System.out.println("Test3");
+=======
     	 System.out.println("Test2");
+>>>>>>> .merge_file_a08332
      }
 }
